df67517f7904d3a7f2845533693c3e87062f77b8
Leonardo Rocha <leonardoleo182@hotmail.com>
2020-04-01 16:39:38 -0300

    Projeto Aluno

---
+++ ProjetoAluno/Entities/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoAluno.Entities
+{
+    class Student
+    {
+        public  int Code { get; set; }
+        public  string Name { get; set; }
+        public  string Matter { get; set; }
+        public double Note1 { get; set; }
+        public double Note2 { get; set; }
+        public double Note3 { get; set; }
+        public double Note4 { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(int code, string name, string matter, double note1, double note2, double note3, double note4)
+        {
+            Code = code;
+            Name = name;
+            Matter = matter;
+            Note1 = note1;
+            Note2 = note2;
+            Note3 = note3;
+            Note4 = note4;
+        }
+
+        public double Average()
+        {
+            return (Note1 + Note2 + Note3 + Note4) / 4;
+        }
+
+        public void Recuperacao(double finalGrade)
+        {
+            if (finalGrade >= 6.00)
+                Console.WriteLine("Approved!");
+            else
+                Console.WriteLine("Disapproved!");
+        }
+    }
+}
---
+++ ProjetoAluno/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using ProjetoAluno.Entities;
+
+namespace ProjetoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter student data");
+            Console.Write("Code: ");
+            int code = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Matter: ");
+            string matter = Console.ReadLine();
+            Console.Write("Note 1: ");
+            double note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 2: ");
+            double note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 3: ");
+            double note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 4: ");
+            double note4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Student s1 = new Student(code, name, matter, note1, note2, note3, note4);
+
+            if (s1.Average() >= 6.00)
+                Console.WriteLine("Approved!");
+            else if (s1.Average() < 6.00 && s1.Average() >= 4.50)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Final Evaluation!");
+                Console.WriteLine();
+                Console.Write("Enter Final Grade: ");
+                double finalGrade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                s1.Recuperacao(finalGrade);
+            }
+            else
+                Console.WriteLine("Disapproved");
+
+            
+            
+
+        }
+    }
+}
